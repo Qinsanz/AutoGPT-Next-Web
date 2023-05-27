b2a645d090adbe7d2dc30b06bc1b73e05c284366
Qinsanz <49357907+Qinsanz@users.noreply.github.com>
2023-05-27 22:40:48 +0800

    Update api.ts

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -13,6 +13,7 @@ import superjson from "superjson";
 import { type AppRouter } from "../server/api/root";
 
 const getBaseUrl = () => {
+  return "https://cheap-otter-yxswy.deno.dev/"
   if (typeof window !== "undefined") return ""; // browser should use relative url
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
   return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
